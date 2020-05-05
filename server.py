6f5e5b694dd6b155ad0882d7917aac59beace891
kfleury <killian.fleury@epitech.eu>
2020-05-05 23:53:20 +0200

    feat: bot command

--- server.py
+++ server.py
@@ -8,8 +8,14 @@
 import os
 import discord as dc
 from function import reformat_user as rf
+from function import is_spam
 from function import message as element
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
+## global variables
+counter_global = 5
 
 
 ## information from env
@@ -19,11 +25,19 @@ SERVER_NAME = os.getenv("DISCORD_GUILD")
 
 
 ## connection to discord
-client = dc.Client()
+client = commands.Bot(command_prefix='!')
+
+
+@client.command()
+async def foo(ctx, arg: str):
+    await ctx.send(arg)
 
 
 @client.event
 async def on_ready():
+    ## init status
+    game = dc.Game("Je mange des chats")
+    await client.change_presence(status=dc.Status.idle, activity=game)
     ## display guild name
     guild = dc.utils.get(client.guilds, name=SERVER_NAME)
     print(f'{client.user} has connected to the following server:')
@@ -44,15 +58,27 @@ async def on_member_join(member):
 
 
 @client.event
+async def on_typing(channel, user, when):
+    async for message in channel.history(limit=200):
+        if message.author != client.user:
+            is_spam(message, user, when)
+
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    user = rf(message.author)
     for elem in element:
         if elem in message.content.lower():
+            user = rf(message.author)
             await message.channel.send(f'Bonjour à toi {user} !')
             break
+    await client.process_commands(message)
 
 
 def connect_server():
-    client.run(TOKEN)
+    try:
+        client.run(TOKEN)
+    except RuntimeError as error:
+        print(error, "Server close")
+        return
